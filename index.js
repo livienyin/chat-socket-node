8ac9abaff449e9736c53dab8dc9bc629e84d2b64
Livien Yin <LivienYin@gmail.com>
2013-05-26 18:14:38 -0700

    Wrote back-end to receive chats

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.get("/", function(req, res){
 app.get("/", function(req, res){
     res.send("It works!");
 });
- 
-app.listen(port);
+
+app.use(express.static(__dirname + '/public')); 
+var io = require('socket.io').listen(app.listen(port));
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
+
 console.log("Listening on port " + port);
