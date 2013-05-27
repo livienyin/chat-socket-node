77560d9ebdf7d4bcc3185db2dba023a7b0f90595
Livien Yin <LivienYin@gmail.com>
2013-05-26 19:36:55 -0700

    Client side js written for messages and senders

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ var port = 3700;
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public')); 
+
 app.get("/", function(req, res){
     res.render("page");
 });
 
-app.get("/", function(req, res){
-    res.send("It works!");
-});
-
-app.use(express.static(__dirname + '/public')); 
 var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection', function (socket) {
